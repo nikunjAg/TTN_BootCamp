a4940c55f2755e9680bd00b67f29383c0c21b24e
Nikunj Aggarwal <nikunj.aggarwal@tothenew.com>
2021-09-26 15:46:53 +0530

    Fixed minor bugs & made headers navs responsive

--- Hotel_Website/app.js
+++ Hotel_Website/app.js
@@ -1,54 +1,21 @@
-const setupHamburger = (
-	navbarSelector,
-	hamburgerSelector,
-	listSelector,
-	extraSelector
-) => {
-	const navbar = document.querySelector(navbarSelector);
-	const hamburger = navbar.querySelector(hamburgerSelector);
-	const list = navbar.querySelector(listSelector);
-	const extra = navbar.querySelector(extraSelector);
+const topHeaderHamburger = document.querySelector(".top-header .hamburger");
+const topHeaderMobileNav = document.querySelector(".top-mobile-nav");
 
-	const showNavbarAndList = (navbar, list, extra) => {
-		navbar.classList.add("d-block");
-		list.classList.add("d-block");
-		extra.classList.add("d-none");
-	};
+const headerHamburger = document.querySelector(".header .hamburger");
+const headerMobileNav = document.querySelector(".header-mobile-nav");
 
-	const hideNavbarAndList = (navbar, list, extra) => {
-		navbar.classList.remove("d-block");
-		list.classList.remove("d-block");
-		extra.classList.remove("d-none");
-	};
+const cancelBtns = document.querySelectorAll(".mobile-nav .close-btn");
 
-	const toggleNavbarAndList = (navbar, list, extra) => {
-		navbar.classList.toggle("d-block");
-		list.classList.toggle("d-block");
-		extra.classList.toggle("d-none");
-	};
+topHeaderHamburger.addEventListener("click", () => {
+	topHeaderMobileNav.classList.toggle("is-active");
+});
 
-	hamburger.addEventListener("click", (event) => {
-		toggleNavbarAndList(navbar, list, extra);
-	});
+headerHamburger.addEventListener("click", () => {
+	headerMobileNav.classList.toggle("is-active");
+});
 
-	// hamburger.addEventListener("mouseenter", (event) => {
-	// 	showNavbarAndList(navbar, list, extra);
-	// });
-
-	// hamburger.addEventListener("mouseleave", (event) => {
-	// 	hideNavbarAndList(navbar, list, extra);
-	// });
-};
-
-setupHamburger(
-	".top-header .top-nav",
-	".hamburger",
-	".top-links-menu",
-	".top-icons-menu"
-);
-setupHamburger(
-	".header .header-nav",
-	".hamburger",
-	".header-links-menu",
-	".search"
+cancelBtns.forEach((cancelBtn) =>
+	cancelBtn.addEventListener("click", () =>
+		cancelBtn.parentElement.classList.remove("is-active")
+	)
 );
