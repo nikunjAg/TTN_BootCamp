19b466251c82aeba6c916ca2b8e74c4e229ff03c
Nikunj Aggarwal <nikunj.aggarwal@tothenew.com>
2021-10-28 19:05:37 +0530

    Combining Reducers in redux

--- 04_React_&_Redux/11-redux/src/index.js
+++ 04_React_&_Redux/11-redux/src/index.js
@@ -1,13 +1,10 @@
-import { createStore } from "redux";
+import { combineReducers, createStore } from "redux";
 
-const INITIAL_STATE = {
+const counterInitState = {
 	counter: 0,
 };
-
-// Reducer
-const counterReducer = (state = INITIAL_STATE, action) => {
-	console.log(action.type);
-
+// Counter Reducer
+const counterReducer = (state = counterInitState, action) => {
 	switch (action.type) {
 		case "INCREMENT": {
 			return { counter: state.counter + 1 };
@@ -15,30 +12,113 @@ const counterReducer = (state = INITIAL_STATE, action) => {
 		case "DECREMENT": {
 			return { counter: state.counter - 1 };
 		}
+		case "DOUBLE": {
+			return { counter: state.counter * 2 };
+		}
 		default:
 			return state;
 	}
 };
 
+const userInitState = {
+	list: [],
+	loading: false,
+};
+// User Reducer
+const userReducer = (state = userInitState, action) => {
+	switch (action.type) {
+		case "ADD_USER": {
+			return { ...state, list: state.list.concat(action.user) };
+		}
+		case "DOUBLE": {
+			return { ...state, loading: true };
+		}
+		default:
+			return state;
+	}
+};
+
+const filmsInitState = {
+	films: [],
+	loading: false,
+	error: null,
+};
+// Films Reducer
+const filmsReducer = (state = filmsInitState, action) => {
+	switch (action.type) {
+		case "pending": {
+			return { ...state, loading: true };
+		}
+		case "success": {
+			return { ...state, films: action.films, error: null };
+		}
+		case "error": {
+			return { ...state, films: [], error: action.error };
+		}
+		default:
+			return state;
+	}
+};
+
+const rootReducer = combineReducers({
+	counter: counterReducer,
+	users: userReducer,
+	films: filmsReducer,
+});
+
 // Store
-const store = createStore(counterReducer);
+const store = createStore(rootReducer);
 
 // Subscriptions
 store.subscribe(() => {
-	console.log("C1: Redux Store Updated: ", store.getState());
-});
-
-store.subscribe(() => {
-	console.log("C2: Redux Store Updated: ", store.getState());
+	console.log("Redux Store Updated: ", store.getState());
 });
 
 console.log(store.getState());
 
+// Middlewares
+
 // Action Creators
 const increment = () => ({ type: "INCREMENT" });
 const decrement = () => ({ type: "DECREMENT" });
+const addUser = (user) => ({ type: "ADD_USER", user });
+const double = () => ({ type: "DOUBLE" });
+
+const sendRequest = (requestConfig, dispatch) => {
+	dispatch({ type: "pending" });
+
+	fetch(requestConfig.url, {
+		method: requestConfig.method || "GET",
+		body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+		headers: requestConfig.headers || {
+			"Content-Type": "application/json",
+		},
+	})
+		.then((response) => response.json())
+		.then((data) => {
+			dispatch({ type: "success", films: data.results });
+		})
+		.catch((e) => {
+			dispatch({ type: "error", error: e });
+		});
+	return { type: "" };
+};
 
 // Dispatching Actions
 store.dispatch(increment());
 store.dispatch(increment());
+
+store.dispatch(addUser({ name: "Nikunj", age: 22 }));
+
 store.dispatch(decrement());
+
+store.dispatch(addUser({ name: "Anmol", age: 25 }));
+store.dispatch(double());
+store.dispatch(addUser({ name: "Harsh", age: 23 }));
+
+store.dispatch(
+	sendRequest(
+		{ url: "https://swapi.dev/api/films", method: "GET" },
+		store.dispatch
+	)
+);
