e3993fe22bd2c1c9e4fb3fc4444d26ae90952337
Nikunj Aggarwal <nikunj.aggarwal@tothenew.com>
2021-11-05 13:01:29 +0530

    Converted User functional component to class-based component

--- 04_React_&_Redux/12-class-based-components/src/components/User.js
+++ 04_React_&_Redux/12-class-based-components/src/components/User.js
@@ -1,7 +1,15 @@
-import classes from './User.module.css';
+import React, { Component } from "react";
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+import styles from "./User.module.css";
+
+class User extends Component {
+	render() {
+		return <li className={styles.user}>{this.props.name}</li>;
+	}
+}
+
+// const User = (props) => {
+//   return <li className={styles.user}>{props.name}</li>;
+// };
 
 export default User;
