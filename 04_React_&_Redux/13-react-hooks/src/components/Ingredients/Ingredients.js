9e0390e252593758d6da6f68d934765e4bcfbe55
Nikunj Aggarwal <nikunj.aggarwal@tothenew.com>
2021-11-08 14:18:38 +0530

    Added firebase to fetch and send ingredients

--- 04_React_&_Redux/13-react-hooks/src/components/Ingredients/Ingredients.js
+++ 04_React_&_Redux/13-react-hooks/src/components/Ingredients/Ingredients.js
@@ -1,21 +1,64 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import IngredientForm from "./IngredientForm";
 import Search from "./Search";
 import IngredientList from "./IngredientList";
 
+const INGREDIENTS_URL =
+	"https://react-hooks-demo-8c722.firebaseio.com/ingredients.json";
+
 function Ingredients() {
 	const [ingredients, setIngredients] = useState([]);
 
-	const addNewIngredientHandler = (ingredientData) => {
-		const newIngredient = {
-			id: Math.random().toString(),
-			...ingredientData,
+	useEffect(() => {
+		const fetchIngredients = async () => {
+			const response = await fetch(INGREDIENTS_URL);
+
+			if (!response.ok) {
+				throw new Error("Somehting went wrong!");
+			}
+
+			const data = await response.json();
+
+			const loadedIngredients = Object.keys(data).map((id) => ({
+				...data[id],
+				id: id,
+			}));
+
+			setIngredients(loadedIngredients);
 		};
 
-		setIngredients((oldIngredients) =>
-			oldIngredients.concat(newIngredient)
-		);
+		fetchIngredients().catch((error) => {
+			console.log(error.message);
+		});
+	}, []);
+
+	const addNewIngredientHandler = (ingredientData) => {
+		fetch(INGREDIENTS_URL, {
+			method: "POST",
+			body: JSON.stringify(ingredientData),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		})
+			.then((response) => {
+				if (!response.ok) throw new Error("Unable to add ingredient!");
+
+				return response.json();
+			})
+			.then((data) => {
+				const newIngredient = {
+					id: data.name,
+					...ingredientData,
+				};
+
+				setIngredients((oldIngredients) =>
+					oldIngredients.concat(newIngredient)
+				);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
 	};
 
 	const removeIngredientHandler = (ingredientId) => {
