e447ec6f268896e23b2cbe12e4fca4434b9f2c81
Nikunj Aggarwal <nikunj.aggarwal@tothenew.com>
2021-11-13 20:24:37 +0530

    Added basic reducers required

---
+++ 04_React_&_Redux/todos/src/App.js
@@ -0,0 +1,8 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+	return <div className="App">App Component</div>;
+}
+
+export default App;
---
+++ 04_React_&_Redux/todos/src/store.js
@@ -0,0 +1,54 @@
+import { combineReducers, createStore } from "redux";
+
+// Concerns with how a particular todo is updated
+// OR state refers to a todo
+const todo = (state, action) => {
+	switch (action.type) {
+		case "ADD_TODO":
+			return { id: action.id, text: action.text, completed: false };
+		case "TOGGLE_TODO": {
+			if (state.id !== action.id) return state;
+			return {
+				...state,
+				completed: !state.completed,
+			};
+		}
+		default:
+			return state;
+	}
+};
+
+// Should only address how todos list is updated
+// OR state refers to todo list
+const todos = (state = [], action) => {
+	switch (action.type) {
+		case "ADD_TODO":
+			return [...state, todo(undefined, action)];
+		case "TOGGLE_TODO":
+			return state.map((t) => todo(t, action));
+		default:
+			return state;
+	}
+};
+
+const visibilityFilter = (state = "", action) => {
+	switch (action.type) {
+		case "SET_VISIBILITY_FILTER":
+			return action.filter;
+		default:
+			return state;
+	}
+};
+
+/* const todoApp = (state = {}, action) => {
+  return {
+    todos: todos(state.todos, action),
+    visibilityFilter: visibilityFilter(state.filter, action)
+  }
+}; */
+const todoApp = combineReducers({
+	todos,
+	visibilityFilter,
+});
+
+const store = createStore(todoApp);
